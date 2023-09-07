6e4c4b78efde9170b81a99a3c99e24fcff799672
Eduardo Cisneros <cisnnerosbcrra@hotmail.com>
2023-09-07 13:43:18 -0600

    Fixed to phone number creation

--- src/api/components/phoneNumbers/controller.js
+++ src/api/components/phoneNumbers/controller.js
@@ -2,13 +2,7 @@ const store = require('./store');
 const boom = require('@hapi/boom');
 
 const phoneNumberCreation = async (data) => {
-    const objPhoneNumber = await store.phoneNumberByNumber(data.phoneNumber);
-
-    if (objPhoneNumber !== null) {
-        throw boom.conflict('Phone number already exist');
-    } else {
-        return await store.phoneNumberCreate(data);
-    }
+    return await store.phoneNumberCreate(data);
 };
 
 const phoneNumberList = async (userId) => {

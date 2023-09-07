e40a80eb80e7b9360d6800e793e96aa0b740661f
Eduardo Cisneros <cisnnerosbcrra@hotmail.com>
2023-09-07 14:02:48 -0600

    UserId get in req.userData

--- src/api/components/phoneNumbers/network.js
+++ src/api/components/phoneNumbers/network.js
@@ -10,8 +10,7 @@ const checkJwt = require('../../../utils/middlewares/auth/checkJwt');
 
 router.get('/list', checkJwt, async (req, res, next) => {
     try {
-        const userId = '64f90ff308f024115c36070e';
-        // const userId = req.userData.id;
+        const userId = req.userData.id;
 
         const data = await controller.phoneNumberList(userId);
 
@@ -30,8 +29,7 @@ router.post(
     validationHandler(addPhoneNumberSchema),
     async (req, res, next) => {
         try {
-            const userId = '64f90ff308f024115c36070e';
-            // const userId = req.userData.id;
+            const userId = req.userData.id;
             const { name, phoneNumber, email, address, group } = req.body;
             const newPhoneNumber = {
                 userId,
